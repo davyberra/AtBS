e049f950d5e4b8a4595e5bd07abdcd8ed642c690
unknown <davyberra@gmail.com>
2021-01-19 23:23:03 -0800

    Finished Web Scraping Chapter

---
+++ download_xkcd.py
@@ -0,0 +1,40 @@
+#! python3
+# download_xkcd.py - Downloads every single XKCD comic.
+
+import requests, os, bs4
+
+
+url = 'https://xkcd.com'        # starting url
+os.makedirs('xkcd', exist_ok=True)  # store comics in ./xkcd
+
+while not url.endswith('#'):
+    # TODO: Download the page.
+    print(f'Downloading page {url}...')
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+    # TODO: Find the URL of the comic image.
+    comicElem = soup.select('#comic img')
+    if comicElem == []:
+        print('Could not find comic image.')
+    else:
+        comicUrl = 'https:' + comicElem[0].get('src')
+
+    # TODO: Download the image.
+    print(f'Downloading image {comicUrl}...')
+    res = requests.get(comicUrl)
+    res.raise_for_status()
+
+    # TODO: Save the image to ./xkcd.
+    imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+    for chunk in res.iter_content(100000):
+        imageFile.write(chunk)
+    imageFile.close()
+
+    # TODO: Get the Prev button's url.
+    prevLink = soup.select('a[rel="prev"]')[0]
+    url = 'https://xkcd.com' + prevLink.get('href')
+
+print('Done. Enjoy your hours of nerdy comics!')
--- searchpypi.py
+++ searchpypi.py
@@ -7,13 +7,14 @@ print('Searching for ' + str(sys.argv[1])) # display text while downloading the 
 res = requests.get('https://google.com/search?q=''https://pypi.org/search/?='
                    + ''.join(sys.argv[1:]))
 res.raise_for_status()
-
+print(sys.argv[1:])
 # Retrieve top search result links.
 
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
 # Open a browser tab for each result.
 linkElems = soup.select('.package-snippet')
+print(linkElems)
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
     urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
